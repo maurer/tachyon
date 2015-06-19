198b5d50335e16054ea9cc1384e5bf4def640fb8
Matthew Maurer <maurer@matthewmaurer.org>
2015-06-19 14:54:09 -0400

    Fixed for modern Data.Bits

--- Util.hs
+++ Util.hs
@@ -3,4 +3,4 @@ module Util where
 import Data.Bits
 
 bitCast :: (Bits a, Bits b) => a -> b
-bitCast x = foldl (.|.) 0 $ map (bit . fst) $ filter snd $ map (\z -> (z, testBit x z)) [0..bitSize x - 1]
+bitCast x = foldl (.|.) zeroBits $ map (bit . fst) $ filter snd $ map (\z -> (z, testBit x z)) [0..bitSize x - 1]
